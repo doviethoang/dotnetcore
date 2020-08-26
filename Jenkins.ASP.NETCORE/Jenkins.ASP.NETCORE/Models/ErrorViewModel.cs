9f4e75b76a4f11b88bea406cde7bf47042fb781a
nghieppv <nghieppv@vnpt.vn>
2020-08-26 09:07:49 +0700

    init .net core

---
+++ Jenkins.ASP.NETCORE/Jenkins.ASP.NETCORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jenkins.ASP.NETCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
